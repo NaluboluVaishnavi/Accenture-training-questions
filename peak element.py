f67ffcc44b0587de54c811fcbc94e8123d5412f0
NaluboluVaishnavi <naluboluvaishnavi@gmail.com>
2024-07-25 20:01:28 +0530

    Add files via upload

---
+++ peak element.py
@@ -0,0 +1,17 @@
+#5
+#1 3 20 4 1
+def peek_element(arr,n):
+    for i in range(n):
+        if i == 0:
+            if arr[i] >= arr[i+1]:
+                return i
+        elif i == n-1:
+            if arr[i] >= arr[i-1]:
+                return i
+        else:
+            if arr[i-1] <= arr[i] and arr[i] >= arr[i+1]: 
+                return i       
+n=int(input())
+arr = list(map(int,input().split(" ")))
+ans=peek_element(arr,n)
+print(ans)
